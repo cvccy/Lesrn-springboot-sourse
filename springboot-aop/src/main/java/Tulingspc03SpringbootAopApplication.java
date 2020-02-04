8f39668ac9b229cc5c14cbfe9867747c37b52958
hanffff <7107233333@qq.com>
2020-02-04 18:09:37 +0800

    springboot aop

---
+++ springboot-aop/src/main/java/Tulingspc03SpringbootAopApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tulingspc03SpringbootAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tulingspc03SpringbootAopApplication.class, args);
+	}
+
+}
---
+++ springboot-aop/src/test/java/cn/hanff/Tulingspc03SpringbootAopApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.hanff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tulingspc03SpringbootAopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
